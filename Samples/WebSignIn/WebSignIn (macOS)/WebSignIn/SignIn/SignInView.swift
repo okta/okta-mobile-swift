4caa48113c9cd9aaf6cf6cfe142ffc668d282aa7
Oleg Gnidets <77254185+oleggnidets-okta@users.noreply.github.com>
2022-02-15 16:58:47 +0200

    Web auth: Sample application for macOS (#16)
    * Initial implementation of Web Authentication sample for macOS
    
    * Improve sample application by using publishers and introduce Details View
    
    * Rename schemes, targets and update folder structure

---
+++ Samples/WebSignIn/WebSignIn (macOS)/WebSignIn/SignIn/SignInView.swift
@@ -0,0 +1,85 @@
+//
+// Copyright (c) 2022-Present, Okta, Inc. and/or its affiliates. All rights reserved.
+// The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+//
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and limitations under the License.
+//
+
+import SwiftUI
+import Combine
+import WebAuthenticationUI
+
+enum AppSettings {
+    enum SignInView {
+        static let height: Double = 400
+        static let width: Double = 500
+    }
+}
+
+struct SignInView: View {
+    @StateObject private var viewModel = SignInViewModel()
+    @Binding private var signedIn: Bool
+    @State private var cancellableSet: Set<AnyCancellable> = []
+    
+    init(signedIn: Binding<Bool>) {
+        self._signedIn = signedIn
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Okta Web Sign In")
+                .font(.largeTitle)
+                .padding()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
+            
+            Text("Have an account?")
+            
+            Button("Sign In") {
+                viewModel.signIn()
+            }
+            .disabled(!viewModel.isConfigured)
+            .alert(isPresented: $viewModel.presentError) {
+                Alert(
+                    title: Text("Error"),
+                    message: Text(viewModel.signInError?.localizedDescription ?? "An unknown error occurred")
+                )
+            }
+            .padding()
+            
+            Toggle("Ephemeral Session", isOn: $viewModel.ephemeralSession)
+            
+            Spacer()
+            
+            Text(viewModel.isConfigured ? "Client ID: \(viewModel.clientID!)" : "Not configured")
+                .font(.caption)
+                .padding()
+        }.onAppear {
+            subscribe()
+        }
+    }
+    
+    func subscribe() {
+        viewModel.$signedIn
+            .sink { self.$signedIn.wrappedValue = $0 }
+            .store(in: &self.cancellableSet)
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView(signedIn: .constant(false))
+            .frame(minWidth: AppSettings.SignInView.width,
+                   minHeight: AppSettings.SignInView.height)
+    }
+}
+#endif
+
+
---
+++ Samples/WebSignIn/WebSignIn (macOS)/WebSignIn/SignIn/SignInViewModel.swift
@@ -0,0 +1,53 @@
+//
+// Copyright (c) 2022-Present, Okta, Inc. and/or its affiliates. All rights reserved.
+// The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+//
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and limitations under the License.
+//
+
+import Foundation
+import Combine
+import WebAuthenticationUI
+import AuthenticationServices
+
+final class SignInViewModel: ObservableObject {
+    var clientID: String? { auth?.flow.configuration.clientId }
+    var isConfigured: Bool { auth?.flow.configuration.clientId != nil }
+    
+    @Published var presentError = false
+    @Published var ephemeralSession = false
+    @Published private(set) var signInError: Error?
+    @Published private(set) var signedIn = Credential.default != nil
+
+    private let auth = WebAuthentication.shared
+    private var cancellableSet: Set<AnyCancellable> = []
+ 
+    init() {
+        $signInError
+            .map { $0 != nil }
+            .assign(to: &$presentError)
+        
+        $ephemeralSession
+            .sink { self.auth?.ephemeralSession = $0 }
+            .store(in: &cancellableSet)
+    }
+    
+    func signIn() {
+        auth?.ephemeralSession = ephemeralSession
+        auth?.start(from: ASPresentationAnchor()) { result in
+            switch result {
+            case .success(let token):
+                Credential.default = Credential(token: token)
+                self.signedIn = true
+            case .failure(let error):
+                self.signInError = error
+                self.signedIn = false
+            }
+        }
+    }
+}
