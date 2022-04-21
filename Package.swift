23ef4217153f9bb6efad0bb6611f1ec054c42520
Mike Nachbaur <mike.nachbaur@okta.com>
2022-04-21 10:24:57 -0700

    Ensure Form conforms to Hashable

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
         .library(name: "WebAuthenticationUI", targets: ["WebAuthenticationUI"])
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(name: "AuthFoundation",
