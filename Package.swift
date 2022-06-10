75dc0a717d1602d96838f486c4ef4b045bb89579
Mike Nachbaur <74688448+mikenachbaur-okta@users.noreply.github.com>
2022-06-10 13:47:18 -0700

    Build and archive DocC documentation (#57)
    * Build zipped docc archives
    
    * Separate GHPages deploy to its own workflow
    
    * Only trigger documentation build when docs have changed
    
    * Push github pages

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
         .library(name: "WebAuthenticationUI", targets: ["WebAuthenticationUI"])
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(name: "AuthFoundation",
