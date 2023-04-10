f365391b341b595cd7bfff9d021ecd9003b1dd34
Mike Nachbaur <74688448+mikenachbaur-okta@users.noreply.github.com>
2023-04-10 10:19:49 -0700

    Ensure the JWK Algorithm cases match the spec (#125)

--- Sources/AuthFoundation/JWT/JWK+Enums.swift
+++ Sources/AuthFoundation/JWT/JWK+Enums.swift
@@ -28,6 +28,7 @@ extension JWK {
     
     /// The algorithm this key is intended to be used with.
     public enum Algorithm: String, Codable {
+        // JWS Algorithms according to https://www.rfc-editor.org/rfc/rfc7518.html#section-3.1
         case hs256 = "HS256"
         case hs384 = "HS384"
         case hs512 = "HS512"
@@ -37,5 +38,28 @@ extension JWK {
         case es256 = "ES256"
         case es384 = "ES384"
         case es512 = "ES512"
+        case ps256 = "PS256"
+        case ps384 = "PS384"
+        case ps512 = "PS512"
+        case none
+        
+        // JWE Algorithms according to https://www.rfc-editor.org/rfc/rfc7518.html#section-4.1
+        case rsa1_5             = "RSA1_5"
+        case rsaOAEP            = "RSA-OAEP"
+        case rsaOAEP256         = "RSA-OAEP-256"
+        case a128KW             = "A128KW"
+        case a192KW             = "A192KW"
+        case a256KW             = "A256KW"
+        case dir                = "dir"
+        case ecdhES             = "ECDH-ES"
+        case ecdhES_a128KW      = "ECDH-ES+A128KW"
+        case ecdhES_a192KW      = "ECDH-ES+A192KW"
+        case ecdhES_a256KW      = "ECDH-ES+A256KW"
+        case a128GCMKW          = "A128GCMKW"
+        case a192GCMKW          = "A192GCMKW"
+        case a256GCMKW          = "A256GCMKW"
+        case pbes2_HS256_A128KW = "PBES2-HS256+A128KW"
+        case pbes2_HS384_A192KW = "PBES2-HS384+A192KW"
+        case pbes2_HS512_A256KW = "PBES2-HS512+A256KW"
     }
 }
