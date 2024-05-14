1a533d88678936466748a1efa463fa79e01955c7
Mike Nachbaur <74688448+mikenachbaur-okta@users.noreply.github.com>
2024-05-14 16:34:43 -0700

    Add support for returning tokens while using MFA Attestation (#191)

--- Sources/AuthFoundation/OAuth2/OAuth2TokenRequest.swift
+++ Sources/AuthFoundation/OAuth2/OAuth2TokenRequest.swift
@@ -13,6 +13,20 @@
 import Foundation
 
 /// Protocol that represents a type of ``APIRequest`` that can be used to exchange a token.
-public protocol OAuth2TokenRequest: APIRequest where ResponseType == Token {
+///
+/// Many different OAuth2 authentication flows can issue tokens, but the types of arguments and their particular workflow can differ. This protocol abstracts the necessary interface for requests that are capable of returning tokens, while allowing the specific arguments and validation steps to be implemented for each unique type of flow.
+public protocol OAuth2TokenRequest: APIRequest, APIRequestBody where ResponseType == Token {
+    /// The application's OAuth2 `client_id` value for this token request.
     var clientId: String { get }
+}
+
+extension OAuth2TokenRequest {
+    private var bodyParameterStrings: [String: String]? {
+        bodyParameters?.compactMapValues({ $0 as? String })
+    }
+    
+    /// Convenience function that exposes an array of ACR Values requested by this OAuth2 token request, if applicable.
+    public var acrValues: [String]? {
+        bodyParameterStrings?["acr_values"]?.components(separatedBy: .whitespaces)
+    }
 }
