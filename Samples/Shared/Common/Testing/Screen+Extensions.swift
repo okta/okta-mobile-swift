0a6f3c71254192e24e638c757c5455098ea3b917
Alex Nachbaur <mike.nachbaur@okta.com>
2025-02-05 15:14:18 -0800

    Another attempt at fixing a CI UI test issue

--- Samples/Shared/Common/Testing/Screen+Extensions.swift
+++ Samples/Shared/Common/Testing/Screen+Extensions.swift
@@ -67,7 +67,7 @@ extension WebLogin where Self: Screen {
                 if !fieldValue.isEmpty {
                     usleep(useconds_t(1000)) // Wait for the field to be selected
                     field.tap(withNumberOfTaps: 3, numberOfTouches: 1)
-                    field.typeKey(XCUIKeyboardKey.delete, modifierFlags: [])
+                    field.typeText("")
                     usleep(useconds_t(500))
                 }
                 
